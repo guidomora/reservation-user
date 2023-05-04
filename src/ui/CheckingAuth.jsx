e84c357706a657ed8058dfd8b1f2e246478267c3
guido morabito <guidomorabito161@hotmail.com>
2023-05-04 19:07:01 -0300

    background img

--- src/ui/CheckingAuth.jsx
+++ src/ui/CheckingAuth.jsx
@@ -11,13 +11,14 @@ const CheckingAuth = () => {
       alignItems="center"
       justifyContent="center"
       sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+      className="firstContainer"
     >
       <Grid
         container
         direction="row"
         justifyContent="center"
       >
-        <CircularProgress color="warning"/>
+        <CircularProgress color="info"/>
       </Grid>
     </Grid>
   );
